9d735548c89850bd8783c8dc3cb9922b5196cc44
matt <wootaejung24@gmail.com>
2025-06-17 09:06:19 +0900

    환경변수에서 전화번호 로드 기능 추가 및 로깅 개선: HAND_TEL1, HAND_TEL2, HAND_TEL3을 환경변수에서 불러오고, 이를 로깅하여 환경변수 확인 기능을 강화함.

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+import os
+
 from PySide6.QtWidgets import QApplication
 
+from custom_logger import logger
 from pick_datetime_view import PickDatetimeView
 from pick_datetime_view_model import PickDatetimeViewModel
 from scraper import IncheonCCScraper
 
+
+def load_hand_tels_from_env():
+    hand_tel1 = os.environ.get("HAND_TEL1")
+    hand_tel2 = os.environ.get("HAND_TEL2")
+    hand_tel3 = os.environ.get("HAND_TEL3")
+    return hand_tel1, hand_tel2, hand_tel3
+
+
 if __name__ == "__main__":
+    # 환경변수 확인
+
+    hand_tel1, hand_tel2, hand_tel3 = load_hand_tels_from_env()
+    logger.info("환경변수 확인")
+    logger.info(f"HAND_TEL1: {hand_tel1}")
+    logger.info(f"HAND_TEL2: {hand_tel2}")
+    logger.info(f"HAND_TEL2: {hand_tel3}")
+
     app = QApplication([])
     scraper = IncheonCCScraper()
     scraper.login()
